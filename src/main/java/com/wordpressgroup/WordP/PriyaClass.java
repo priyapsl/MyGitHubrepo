5bfa3f6c62deb2784f8b68baaf33f44e84c45251
priya <priya@LAPTOP-H5PVVSBB>
2018-04-27 09:49:22 +0530

    "This is the second commit on 002"

---
+++ src/main/java/com/wordpressgroup/WordP/PriyaClass.java
@@ -0,0 +1,11 @@
+package com.wordpressgroup.WordP;
+
+public class PriyaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" This is the new class from priya");
+	}
+
+}
